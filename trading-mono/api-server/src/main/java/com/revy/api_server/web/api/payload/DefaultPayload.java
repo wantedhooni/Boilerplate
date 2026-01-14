bdbcc71ada4b6eba11e0a23e2e02ae12f2f73399
Revy <wantedhooni@gmail.com>
2026-01-15 00:07:40 +0900

    소스 정리

--- trading-mono/api-server/src/main/java/com/revy/api_server/web/api/payload/DefaultPayload.java
+++
@@ -1,6 +0,0 @@
-package com.revy.api_server.web.api.payload;
-
-public class DefaultPayload {
-    public record Res(String message) {
-    }
-}
--- trading-mono/api-server/src/main/java/com/revy/api_server/web/service/dto/LoginCommand.java
+++
@@ -1,6 +0,0 @@
-package com.revy.api_server.web.service.dto;
-
-public interface LoginCommand {
-    public String getEmail();
-    public String getPassword();
-}
