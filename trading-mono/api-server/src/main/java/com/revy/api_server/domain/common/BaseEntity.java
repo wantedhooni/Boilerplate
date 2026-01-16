6b922144989d7bb6ae2b5503b0efd882212285e5
Revy <wantedhooni@gmail.com>
2026-01-16 13:41:39 +0900

    코트 리팩토링

--- trading-mono/api-server/src/main/java/com/revy/api_server/domain/common/BaseEntity.java
+++ trading-mono/api-server/src/main/java/com/revy/api_server/domain/common/BaseEntity.java
@@ -1,13 +1,11 @@
 package com.revy.api_server.domain.common;
 
-import com.github.f4b6a3.uuid.UuidCreator;
 import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
-import jakarta.persistence.PrePersist;
 import lombok.Getter;
 import lombok.ToString;
 import org.springframework.data.annotation.CreatedDate;
@@ -15,7 +13,6 @@ import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
-import java.util.UUID;
 
 
 @Getter
@@ -28,15 +25,15 @@ public abstract class BaseEntity<T> {
     @Column(name="id")
     private T id;
 
-    @Column(name="publicId", nullable = false, unique = true, columnDefinition = "UUID")
-    private UUID publicId;         // UUID v7 (외부 노출)
-
-    @PrePersist
-    void prePersist() {
-        if (this.publicId == null) {
-            this.publicId = UuidCreator.getTimeOrderedEpoch();
-        }
-    }
+//    @Column(name="publicId", nullable = false, unique = true, columnDefinition = "UUID")
+//    private UUID publicId;         // UUID v7 (외부 노출)
+//
+//    @PrePersist
+//    void prePersist() {
+//        if (this.publicId == null) {
+//            this.publicId = UuidCreator.getTimeOrderedEpoch();
+//        }
+//    }
 
 //    @CreatedBy
 //    @Column(name = "created_by", nullable = false, length = 50, updatable = false)
